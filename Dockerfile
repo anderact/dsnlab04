7356c73336719780d4b43dbb2cb82ec2352e4f0d
Axel Chacon <85888364+anderact@users.noreply.github.com>
2023-04-17 21:54:19 -0500

    actual first commit

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+#imagen base de Node 19
+FROM node:19
+
+#clonación del repositorio
+RUN apk update && apk add git
+RUN git clone https://github.com/anderact/dsnlab04.git /usr/src/app
+
+#definición del directorio de trabajo
+WORKDIR /usr/src/app
+
+#se descargan las dependencias en la ruta del directorio de trabajo
+RUN npm install --prefix /usr/src/app
+
+#se expone el puerto solicitado
+EXPOSE 9000
+
+#comando a ejecutar
+CMD ["npm", "start", "--prefix", "/usr/src/app"]
---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+
+const port = 5000
+
+app.get('/', (req, res) => {
+    res.send('Laboratorio 04')
+})
+
+app.get('/clientes', (req, res) => {
+    const clientes = ['Axel', 'Ander', 'Otro']
+    res.send(clientes)
+})
+
+app.get('/productos', (req, res) => {
+    const productos = ['Pera', 'Manzana', 'Uva']
+    res.send(productos)
+})
+
+app.listen(port, () => {
+    console.log(`Aplicación corriendo en ${port}`)
+})
